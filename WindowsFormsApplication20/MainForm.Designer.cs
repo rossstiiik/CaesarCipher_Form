e3bc314d227591f112dc705433e3de33c5406457
rossstiiik <hlebnikov.rostislav@yandex.ru>
2025-06-17 15:45:36 +0300

    first commit
    first commit

---
+++ WindowsFormsApplication20/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace CaesarCipher
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 25);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(360, 80);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 142);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(360, 80);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 228);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Зашифровать";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(93, 228);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Расшифровать";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текст:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Сдвиг:";
+            // 
+            // txtShift
+            // 
+            this.txtShift.Location = new System.Drawing.Point(70, 105);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(50, 20);
+            this.txtShift.TabIndex = 7;
+            this.txtShift.Text = "3";
+            this.txtShift.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtShift_KeyPress);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 263);
+            this.Controls.Add(this.txtShift);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "MainForm";
+            this.Text = "Шифр Цезаря";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtShift;
+    }
+}
---
+++ WindowsFormsApplication20/MainForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaesarCipher
+{
+   
+    public partial class MainForm : Form
+    {
+        private string russianSmall = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private string russianBig = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        private string englishSmall = "abcdefghijklmnopqrstuvwxyz";
+        private string englishBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     //   public bool isRussianSmall = false;
+      //  public bool isRussianBig = false;
+     //   public bool isEnglishSmall = false;
+     //   public bool isEnglishBig = false;
+    //    public int alphabet;
+        
+        public int shift;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtShift.Text, out shift)) // Объявляем shift здесь
+            {
+                MessageBox.Show("Введите целое число для сдвига!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+                 
+            }
+            
+            txtOutput.Text = CaesarCipher(txtInput.Text, shift); // Используем shift
+            //alphabet = 0;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtShift.Text, out shift)) // Объявляем shift здесь
+            {
+                MessageBox.Show("Введите целое число для сдвига!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+            txtOutput.Text = CaesarCipher(txtInput.Text, -shift); // Используем shift с минусом
+          
+        }
+
+        private string CaesarCipher(string text, int shift)
+        {
+            char[] result = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                string currentAlphabet = null;
+
+                // Определяем, к какому алфавиту принадлежит символ
+                if (russianSmall.Contains(c.ToString()))
+                {
+                    currentAlphabet = russianSmall;
+                    
+                } 
+                else if (russianBig.Contains(c.ToString()))
+                {
+                    currentAlphabet = russianBig; 
+                    
+                }
+                    
+                else if (englishSmall.Contains(c.ToString()))
+                {
+                    currentAlphabet = englishSmall;
+                    
+                }
+
+                else if (englishBig.Contains(c.ToString()))
+                {
+                    currentAlphabet = englishBig;
+                    
+                }
+                    
+
+                if (currentAlphabet != null)
+                {
+                    // Применяем сдвиг только по своему алфавиту
+                    int index = currentAlphabet.IndexOf(c);
+                    int newIndex = (index + shift) % currentAlphabet.Length;
+                    if (newIndex < 0)
+                        newIndex += currentAlphabet.Length;
+                    result[i] = currentAlphabet[newIndex];
+                }
+                else
+                {
+                    // Оставляем символы, не входящие в алфавиты
+                    result[i] = c;
+                }
+            }
+
+            return new string(result);
+           // alphabet = 0;
+        }
+
+        // Ограничиваем ввод в txtShift только цифрами и минусом
+        private void txtShift_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+
+            // Запрещаем минус не в начале
+            if (e.KeyChar == '-' && ((TextBox)sender).SelectionStart != 0)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
